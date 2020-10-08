f2e784e4d837b199da46a5ca84363ad38adad3a4
angiegonza125 <69263578+angiegonza125@users.noreply.github.com>
2020-10-08 16:13:57 -0600

    final

---
+++ main.js
@@ -0,0 +1,14 @@
+document .getElementById("red").addEventListener("click", function(){
+    document.getElementById('screen').style.backgroundColor = "red";
+});
+document .getElementById("green").addEventListener("click", function(){
+    document.getElementById('screen').style.backgroundColor = "green";
+});
+
+document .getElementById("blue").addEventListener("click", function(){
+    document.getElementById('screen').style.backgroundColor = "blue";
+});
+
+document .getElementById("btn").addEventListener("click", function(){
+    document.getElementById('screen').style.backgroundColor = "rgb(99, 114, 129)";
+});
